ef6256bfb12b63a753695635ed9288a05146aa4d
Rogerio234 <rogeriojss@gmail.com.br>
2023-10-02 17:08:24 -0300

    correcoes para deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import styled from "styled-components"
 import CoresTemaEscuro from "/src/colors/cores.js"
 import Login from "./Componentes/Login"
 import EstilosGlobais from "./Componentes/EstilosGlobais/estilosGlobais"
-import { BrowserRouter as Router,Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router,Routes, Route,HashRouter } from "react-router-dom"
 import Home from "./Componentes/Home"
 
 
 function App() {
   
   return (
-    <Router>
-      <div>
-        <EstilosGlobais/>
-        <Routes>
-          <Route path="/Projeto-Integrador/login" element={<Login/>}/>
-          <Route path="/Projeto-Integrador" element={<Home/>}/>
-        </Routes>
-      </div>
-    </Router>
+    <HashRouter>
+      <Router>
+        <div>
+          <EstilosGlobais/>
+          <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/Projeto-Integrador" element={<Home/>}/>
+          </Routes>
+        </div>
+      </Router>
+    </HashRouter>
   )
 }
 
